22efcd3068dc9078efae4eed1e4d267288edb1c4
joao <simp4kda@gmail.com>
2025-06-05 11:43:48 -0300

    class

--- aula49(cuidados com tipos de dados mutaveis (list e copy)).py
+++ aula49(cuidados com tipos de dados mutaveis (list e copy)).py
@@ -5,10 +5,23 @@ Cuidados com dados mutaveis
 '''
 
 lista_a = ['Luiz', 'Gaby']
-lista_b = lista_a
+lista_b = lista_a.copy() # Retorna uma nova lista a partir da lista_a para a lista_b
 
+# OU seja, se a gente fizer qualquer alteracao na lista_a, essa alteracao nao acontecera
+# na lista_b, pois a lista_b e uma nova lista.
+
+lista_a[0] = 500
+
+print(lista_a)
 print(lista_b)
 
-lista_b = lista_a[0]
+# Agora se so copiarmos a lista_a para a lista_b, qualquer alteracao que fizermos na lista_a
+# vai ser feito tambem na lista_b, porque a lista_b e a lista_a
 
+lista_a = ['Luiz', 'Gaby']
+lista_b = lista_a
+
+lista_a[0] = 500
+
+print(lista_a)
 print(lista_b)
